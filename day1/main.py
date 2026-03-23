b96af895d8905724f6b98aabdf38425884460639
Konarkpreserve <konark.sahu@ssipmt.com>
2026-03-23 21:53:41 +0530

    d2 and cors added

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import time
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class mockgem_res:
     def __init__(self, text):
