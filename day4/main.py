40dd8ba65a43fecb7d4310f38af5fef835ca7b13
Konarkpreserve <konark.sahu@ssipmt.com>
2026-03-31 23:44:22 +0530

    d5 -history_lim fix + ux improve

--- day4/main.py
+++ day4/main.py
@@ -13,15 +13,22 @@ app.add_middleware(
 )
 
 chat_history=[]
+MAX_HISTORY=10
 
 @app.get("/ask")
 def ask(q: str):
+    global chat_history
     
     chat_history.append({"user": q})   #store message
 
     answer = get_ai_res(q)
 
     chat_history.append({"ai": answer}) #store ai res
+
+    while len(chat_history)>MAX_HISTORY:
+        chat_history.pop(0)
+
+        print("hist_len", len(chat_history))
 
     return {
         "answer": answer,
