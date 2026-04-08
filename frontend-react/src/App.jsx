d3a0bfb4b8d8f57819fe0a2524d8608e5aa283e9
Konarkpreserve <konark.sahu@ssipmt.com>
2026-04-08 23:10:01 +0530

    day11 Style added

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useState } from "react";
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
 
     const newMessages=[...messages, {role: "user", text: message}];
     setMessages(newMessages)
+
+    setMessages([
+      ...newMessages,
+      {role:"ai", text: "Thinking...."}
+    ]);
 
     try{
       let res = await fetch(`http://127.0.0.1:8000/ask?q=${encodeURIComponent(message)}`);
@@ -39,23 +45,27 @@ function App() {
   }
 
   return (
-    <div>
+    <div className="chat-container">
       <h2>AI CHAT N</h2>
 
-    <div>
+    <div className="chat-box">
       {messages.map((msg, index) => (
-        <p key={index}>
-          <b>{msg.role === "user" ? "You" : "AI"}:</b> {msg.text}
-        </p>
+        <div key={index} className={msg.role === "user" ? "user" : "ai"} >
+           <span>{msg.text}</span>
+        </div>
       ))}
     </div>
 
-      <input
-      value={message}
-      onChange={(e) => setmessage(e.target.value)} 
-      placeholder="Type Something...." />
-      
+    <div className="input-box">
+       <input
+       value={message}
+       onChange={(e) => setmessage(e.target.value)}
+       placeholder="Type Your Thoughts..."
+       />
+
       <button onClick={askAI}>Send</button>
+
+      </div>
 
       {/* <p><b>AI:</b> {response}</p>  */} 
     </div>
